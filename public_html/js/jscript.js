6afae1cb4495beafd855be9b524a2c6d354a8ef8
Ventsi <ven.iv@gmx.com>
2015-11-17 10:25:13 +0200

    - Add a custom js script - Make the front-end layout full window height

---
+++ public_html/js/jscript.js
@@ -0,0 +1,13 @@
+$(function(){
+    //position full page height
+    $('#main').css({
+        minHeight: window.innerHeight,
+        position: 'relative'
+    });
+    $('#footer').css({
+        position: 'absolute',
+        bottom:0
+    });
+
+
+});
