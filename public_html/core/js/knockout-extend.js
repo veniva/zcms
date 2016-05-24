9f49230e0d6271ddaf6bb20231601f42fe7666ca
Ventsi <ven.iv@gmx.com>
2016-05-24 10:48:46 +0300

    - Add a knockout event binding for stop bubbling

--- public_html/core/js/knockout-extend.js
+++ public_html/core/js/knockout-extend.js
@@ -1,7 +1,20 @@
+//enables the parsing of knockout bindings inserted with the new HTML
 ko.bindingHandlers['dynamicHtml'] = {
     'update': function (element, valueAccessor, allBindings, viewModel, bindingContext) {
         // setHtml will unwrap the value if needed
         ko.utils.setHtml(element, valueAccessor());
         ko.applyBindingsToDescendants(bindingContext, element);
+    }
+};
+
+//prevents the execution of the parent event when clicked onto a child.
+ko.bindingHandlers.stopBubble = {
+    init: function(element) {
+        ko.utils.registerEventHandler(element, "click", function(event) {
+            event.cancelBubble = true;
+            if (event.stopPropagation) {
+                event.stopPropagation();
+            }
+        });
     }
 };
