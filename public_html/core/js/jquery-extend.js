694890849450c4a5d2f9c50d83baead763d225c3
Ventsi <ven.iv@gmx.com>
2016-08-23 19:54:52 +0300

    - Adjust the no scroll option's height.

--- public_html/core/js/jquery-extend.js
+++ public_html/core/js/jquery-extend.js
@@ -185,6 +185,8 @@ $.ui.dialog.prototype.open = function () {
 
         if(self.options.noScroll){
             elem.css('overflow', 'hidden');
+            var noScrollHeight = setHeight+40;
+            elem.dialog("option", "height", noScrollHeight).parent().css("max-height", noScrollHeight);
         }
     };
 
